457b7c7a78745d3a5f1d78daaa92d797bfa4a444
talal-sharaa <talal.i.sharaa@gmail.com>
2024-05-13 13:24:50 +0300

    chore: Refactor IP display functionality in index.html and main.js

--- Task9/main.js
+++ Task9/main.js
@@ -1,7 +1,11 @@
-fetch("https://api.ipify.org/")
-  .then((response) => response.text())
-  .then((ip) => {
-    console.log(ip);
-    document.getElementById("IP").innerHTML = "<p>" + ip + "</p>";
-  })
-  .catch((error) => console.error("Error:", error));
+document.getElementById("IP").addEventListener("click", getIP);
+
+function getIP() {
+  fetch("https://api.ipify.org/")
+    .then((response) => response.text())
+    .then((ip) => {
+      console.log(ip);
+      document.getElementById("IP").innerHTML = "<p>" + ip + "</p>";
+    })
+    .catch((error) => console.error("Error:", error));
+}
